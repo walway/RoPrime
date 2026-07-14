4afc50b07fb1fdff2b3d8eaa1d2569939d71c483
Kevin <evgesnabk@gmail.com>
2026-07-14 02:57:32 -0700

    Added sandbox

---
+++ src/content/ui/pill.js
@@ -0,0 +1,20 @@
+<div class="MuiGrid-root web-blox-css-tss-spvy06-Grid-root MuiGrid-item MuiGrid-grid-XSmall-12 web-blox-css-mui-wa23gu" style="margin-top: 16px;">
+   <div role="radiogroup" aria-label="Audience" tabindex="0" class="fw-segmented-control flex items-center width-[fit-content] relative padding-xsmall gap-xxsmall radius-medium bg-shift-200">
+      <div class="fw-segmented-control-indicator absolute bg-shift-300 transition-all height-800 radius-small" aria-hidden="true" style="width: 80px; left: 168px; height: 32px; top: 4px; transform: none; opacity: 1;"></div>
+      <button type="button" role="radio" aria-checked="false" class="relative clip group/interactable focus-visible:outline-focus disabled:outline-none flex items-center justify-center transition-colors stroke-none relative bg-none fill min-width-[80px] max-width-[200px] height-800 padding-x-medium radius-small cursor-pointer">
+         <div role="presentation" class="absolute inset-[0] transition-colors group-hover/interactable:bg-[var(--color-state-hover)] group-active/interactable:bg-[var(--color-state-press)] group-disabled/interactable:bg-none"></div>
+         <div class="flex items-center justify-center relative width-full content-default text-label-medium" style="overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"><span class="padding-y-xsmall text-truncate-end text-no-wrap">Button 1</span></div>
+      </button>
+      <button type="button" role="radio" aria-checked="false" class="relative clip group/interactable focus-visible:outline-focus disabled:outline-none flex items-center justify-center transition-colors stroke-none relative bg-none fill min-width-[80px] max-width-[200px] height-800 padding-x-medium radius-small cursor-pointer">
+         <div role="presentation" class="absolute inset-[0] transition-colors group-hover/interactable:bg-[var(--color-state-hover)] group-active/interactable:bg-[var(--color-state-press)] group-disabled/interactable:bg-none"></div>
+         <div class="flex items-center justify-center relative width-full content-default text-label-medium" style="overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"><span class="padding-y-xsmall text-truncate-end text-no-wrap">Button 2</span></div>
+      </button>
+      <button type="button" role="radio" aria-checked="true" class="relative clip group/interactable focus-visible:outline-focus disabled:outline-none flex items-center justify-center transition-colors stroke-none relative bg-none fill min-width-[80px] max-width-[200px] height-800 padding-x-medium radius-small cursor-pointer">
+         <div role="presentation" class="absolute inset-[0] transition-colors group-hover/interactable:bg-[var(--color-state-hover)] group-active/interactable:bg-[var(--color-state-press)] group-disabled/interactable:bg-none"></div>
+         <div class="flex items-center justify-center relative width-full content-emphasis text-label-medium" style="overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"><span class="padding-y-xsmall text-truncate-end text-no-wrap">Button 3</span></div>
+      </button>
+   </div>
+</div>
+
+aria-checked="false"
+aria-checked="true"
---
+++ src/content/ui/tooltip.js
@@ -0,0 +1,5 @@
+<div role="tooltip" id="_r_2p_" class="MuiPopper-root MuiTooltip-popper MuiTooltip-popperInteractive MuiTooltip-popperArrow web-blox-css-mui-l66bl4" style="position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate(408px, 364px);" data-popper-placement="bottom">
+  <div class="MuiTooltip-tooltip web-blox-css-tss-t4ajt1-Typography-tooltip MuiTooltip-tooltipArrow MuiTooltip-tooltipPlacementBottom web-blox-css-mui-1x77lqp" style="opacity: 1; transform: none; transition: opacity 200ms cubic-bezier(0.4, 0, 0.2, 1), transform 133ms cubic-bezier(0.4, 0, 0.2, 1);">Unique users who join your game daily. <span class="MuiTooltip-arrow web-blox-css-tss-1deuxot-Tooltip-arrow web-blox-css-mui-1urvb1y" style="position: absolute; left: 0px; transform: translate(129px, 0px);"></span>
+  </div>
+</div>
+
