5f6fe35221a0a9996a94f09b2316d8b84c72c218
Kevin <evgesnabk@gmail.com>
2026-07-21 07:28:33 -0700

    Compatibility fixes

---
+++ src/content/profile/profileRedesign.js
@@ -0,0 +1,27 @@
+.profile-header-overlay {
+    margin-top: -80px !important;
+}
+
+.profile-avatar-left {
+    background-color: transparent;
+}
+
+.cover-blur-overlay {
+    display: none;
+}
+
+.cover-gradient-overlay {
+    display: none;
+}
+
+.profile-avatar-left .profile-avatar-gradient {
+  display: none;
+}
+
+.profile-currently-wearing {
+  display: none;
+}
+
+.avatar-toggle-button {
+    display: none;
+}
